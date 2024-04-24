5d39fe113ac935feb33052561ee19c7ea672830a
Dominik Eis <146200413+Eisik02@users.noreply.github.com>
2024-04-24 11:07:39 +0200

    Update tb_top_level.vhdl

--- tb_top_level.vhdl
+++ tb_top_level.vhdl
@@ -3,55 +3,45 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity top_level_tb is
--- This test bench entity does not have ports.
 end top_level_tb;
 
-architecture behavior of top_level_tb is 
-    -- Component Declaration for the Unit Under Test (UUT)
+architecture behavior of top_level_tb is
+
+    -- Component Declaration for the top_level
     component top_level
         Port (
-            CLK100MHZ : in  std_logic;
-            BTNC      : in  std_logic;
-            echo1     : in  std_logic;
-            echo2     : in  std_logic;
-            trig1     : out std_logic;
-            trig2     : out std_logic;
-            CA        : out std_logic;
-            CB        : out std_logic;
-            CC        : out std_logic;
-            CD        : out std_logic;
-            CE        : out std_logic;
-            CF        : out std_logic;
-            CG        : out std_logic;
-            DP        : out std_logic;
+            CLK100MHZ : in  std_logic;                   
+            BTNC      : in  std_logic;                    
+            echo1     : in  std_logic;                    
+            echo2     : in  std_logic;                    
+            trig1     : out std_logic;                   
+            trig2     : out std_logic;                  
+            CA        : out std_logic;                   
+            CB        : out std_logic;                    
+            CC        : out std_logic;                   
+            CD        : out std_logic;                   
+            CE        : out std_logic;                    
+            CF        : out std_logic;                  
+            CG        : out std_logic;                
+            DP        : out std_logic;                  
             AN        : out std_logic_vector(7 downto 0);
             LED16_G   : out std_logic;
             LED16_R   : out std_logic;
             LED17_G   : out std_logic;
             LED17_R   : out std_logic;
-            display_select : in std_logic;
-            sig_out : out std_logic_vector(15 downto 0)
+            display_select : in std_logic
         );
     end component;
 
-    -- Input signals
-    signal CLK100MHZ : std_logic := '0';
-    signal BTNC : std_logic := '0';
-    signal echo1 : std_logic := '0';
-    signal echo2 : std_logic := '0';
-    signal display_select : std_logic := '0';
-
-    -- Output signals
-    signal trig1, trig2 : std_logic;
-    signal CA, CB, CC, CD, CE, CF, CG, DP : std_logic;
+    signal CLK100MHZ, BTNC, echo1, echo2, display_select : std_logic := '0';
+    signal trig1, trig2, CA, CB, CC, CD, CE, CF, CG, DP : std_logic;
     signal AN : std_logic_vector(7 downto 0);
     signal LED16_G, LED16_R, LED17_G, LED17_R : std_logic;
-    signal sig_out : std_logic_vector(15 downto 0);
 
-    -- Clock period definitions
     constant clk_period : time := 10 ns;
 
 begin
+
     -- Instantiate the Unit Under Test (UUT)
     uut: top_level
         port map (
@@ -74,43 +64,53 @@ begin
             LED16_R => LED16_R,
             LED17_G => LED17_G,
             LED17_R => LED17_R,
-            display_select => display_select,
-            sig_out => sig_out
+            display_select => display_select
         );
 
     -- Clock process definitions
-    clk_process : process
+    clock_process: process
     begin
-        CLK100MHZ <= '0';
-        wait for clk_period/2;
-        CLK100MHZ <= '1';
-        wait for clk_period/2;
+        while True loop
+            CLK100MHZ <= '0';
+            wait for clk_period / 2;
+            CLK100MHZ <= '1';
+            wait for clk_period / 2;
+        end loop;
     end process;
 
     -- Stimulus process
-    stim_proc : process
-    begin  
-        -- Initialize Inputs
-        BTNC <= '1';
-        wait for clk_period*10;  -- Wait for the reset to take effect
-        BTNC <= '0';
+    stim_proc: process
+    begin
 
-        wait for clk_period*10;
+        BTNC <= '1';
+        wait for 100 ns;
+        BTNC <= '0';
+        wait for 100 ns;
+
+        trig1 <= '1';
+        wait for 10 ns;
+        trig1 <= '0';
+        wait for 10 ns;
         echo1 <= '1';
-        wait for clk_period*5;
+        wait for 30 ns; 
         echo1 <= '0';
 
-        wait for clk_period*10;
+   
+        trig2 <= '1';
+        wait for 10 ns;
+        trig2 <= '0';
+        wait for 15 ns; 
         echo2 <= '1';
-        wait for clk_period*5;
+        wait for 40 ns;
         echo2 <= '0';
 
-        wait for clk_period*10;
-        display_select <= '1'; -- Change display output
-
-        wait for clk_period*10;
-        display_select <= '0'; -- Change display output back
+   
+        display_select <= '1'; 
+        wait for 100 ns;
+        display_select <= '0';
+        wait for 100 ns;
 
         wait;
     end process;
+
 end behavior;
